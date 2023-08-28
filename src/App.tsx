668bdafa85db41ad610108e2d3b7e6861fb48c54
HabibMollah <hrmollah2002@gmail.com>
2023-08-29 01:53:14 +0600

    passed all tests

--- src/App.tsx
+++ src/App.tsx
@@ -11,18 +11,6 @@ export default function App() {
   const [error, setError] = useState('');
   const [quote, setQuote] = useState<Quote>();
 
-  useEffect(() => {
-    axios
-      .get(
-        'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json'
-      )
-      .then((res) => setData(res.data.quotes))
-      .catch((err) => {
-        setError('Something went wrong, please reload this page');
-        console.error(err);
-      });
-  }, []);
-
   function randomIndex() {
     if (data) return Math.floor(Math.random() * data.length);
     return 0;
@@ -33,20 +21,52 @@ export default function App() {
     return;
   }
 
+  useEffect(() => {
+    axios
+      .get(
+        'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json'
+      )
+      .then((res) => {
+        setData(res.data.quotes);
+        randomQuote();
+      })
+      .catch((err) => {
+        setError('Something went wrong, please reload this page');
+        console.error(err);
+      });
+  }, []);
+
   console.log(data);
 
   return (
-    <>
-      <main id="quote-box">
-        <section id="text">{quote?.quote}</section>
-        <h2 id="author">{quote?.author}</h2>
-        <button onClick={randomQuote} id="new-quote">
-          New Quote
-        </button>
-        <a href="" id="tweet-quote"></a>
+    <div className="w-[100dvw] h-[100dvh]">
+      <main
+        className="mx-auto p-4 rounded-lg max-w-[640px] bg-green-800 text-white"
+        id="quote-box">
+        <p className="text-4xl font-medium" id="text">
+          ❝{quote?.quote}❞
+        </p>
+        <p className="text-right text-xl font-medium" id="author">
+          - {quote?.author}
+        </p>
+        <div className="flex justify-center items-center gap-8 font-medium">
+          <button
+            className="bg-white text-black rounded-lg px-8"
+            onClick={randomQuote}
+            id="new-quote">
+            New Quote
+          </button>
+          <a
+            className="bg-white text-black rounded-lg px-8"
+            href="https://twitter.com/intent/tweet"
+            target="_blank"
+            id="tweet-quote">
+            Tweet
+          </a>
+        </div>
         {/* error message */}
         <p className="text-red-500">{error}</p>
       </main>
-    </>
+    </div>
   );
 }
